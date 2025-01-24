f62888e50f491721e405cc6a0344c8cacec2f8e5
Yvan Sabay <sabayyvan2018@gmail.com>
2025-01-24 15:53:49 +0800

    added dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/test-utils/module',
+    '@nuxt/eslint'
+  ]
 })
