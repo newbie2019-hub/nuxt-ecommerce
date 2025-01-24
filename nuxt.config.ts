84a1a5061932428440fec1952fa2bf50ced38dbc
Yvan Sabay <sabayyvan2018@gmail.com>
2025-01-24 15:56:46 +0800

    added nuxt google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/image',
